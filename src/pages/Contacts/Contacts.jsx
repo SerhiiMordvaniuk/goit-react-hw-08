2c0efc0317576b6e056b5c033978a03da4cd62b6
Serhii Mordvaniuk <sm636332@gmail.com>
2025-02-04 17:51:58 +0100

    response

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearschBox from "../../components/SearchBox/SearschBox";
 import ContactList from "../../components/ContactList/ContactList";
+import s from "./Contacts.module.css";
 
 const Contacts = () => {
   return (
     <main className="container">
-      {" "}
-      <ContactForm />
-      <SearschBox />
-      <ContactList />
+      <div className={s.contacts}>
+        {" "}
+        <ContactForm />
+        <SearschBox />
+        <ContactList />
+      </div>
     </main>
   );
 };
